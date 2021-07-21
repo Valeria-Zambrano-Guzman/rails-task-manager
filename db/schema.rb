b00fed3988c67badbf254d6d643b02455642c8e9
valeria zambrano <Valeria.zambrano.guzman@gmail.com>
2021-07-21 15:06:26 -0400

    change column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_183828) do
+ActiveRecord::Schema.define(version: 2021_07_21_190339) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

727b3a030f0183ae3ac3d8621ac36d4d5c1dca85
valeria zambrano <Valeria.zambrano.guzman@gmail.com>
2021-07-22 15:22:42 -0400

    Html Finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,10 @@
 class TasksController < ApplicationController
+    before_action :find_task,  only: [:show, :edit, :update, :destroy]
+
     def index
         @tasks = Task.all
     end
     def show
-        @task = Task.find(params[:id])
     end
     def new
         @task = Task.new 
@@ -14,24 +15,25 @@ class TasksController < ApplicationController
         redirect_to tasks_path(@task)
     end
     def edit
-        @task = Task.find(params[:id])
     end
     def update
-        @task = Task.find(params[:id])
         @task.update(tasks_params)
         redirect_to tasks_path(@task)
     end
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
     
         # no need for app/views/restaurants/destroy.html.erb
         redirect_to tasks_path
-      end
+    end
   
     private
   
     def tasks_params
       params.require(:task).permit(:title, :details, :completed)
+    end
+
+    def find_task
+      @task = Task.find(params[:id])  
     end
 end
